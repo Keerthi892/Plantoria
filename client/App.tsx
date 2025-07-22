6be33e5bcbc665b130c436ff8a2c0b13acf54825
Keerthi892 <s190087@rguktsklm.ac.in>
2025-07-22 16:48:54 +0530

    commit: vite fix1

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./global.css";
 
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "../client/components/ui/toaster";
 import { createRoot } from "react-dom/client";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "../client/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
